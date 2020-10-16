c221f70e2aead7f6fc04f88ee824ec1a1e4e3761
ali <ali@gmail.com>
2020-10-16 02:14:18 -0700

    api for get&post

---
+++ core/views.py
@@ -0,0 +1,40 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# third party imports
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+#
+from .serializers import PostSerializer
+#
+
+from .models import Post
+# Create your views here.
+
+class TestView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        qs = Post.objects.all()
+        post = qs.first()
+        #serializer = PostSerializer(qs, many=True)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
+
+# def test_view(request):
+#     data = {
+#         'name' : 'Ali',
+#         'age' : 23
+#     }
+#     return JsonResponse(data)
